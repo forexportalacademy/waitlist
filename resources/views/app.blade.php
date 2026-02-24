0f440fa8599d4292f60043a6670e8880903b3064
Kenneth Ekandem <kennyekandem@gmail.com>
2026-02-24 19:19:44 +0100

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,11 +8,13 @@
         <title inertia>{{ config('app.name', 'Waitlist') }}</title>
         
         <!-- Favicon -->
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png?v=2">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png?v=2">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2">
+        <link rel="shortcut icon" href="/fp-favicon.ico">
+        <meta name="msapplication-TileColor" content="#1b3e71">
+        <meta name="theme-color" content="#1b3e71">
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
